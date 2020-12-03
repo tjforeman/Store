d4ac4e1472086d9b22328ffee35f07edb03fb919
tjforeman <tylerforeman92@gmail.com>
2020-12-02 23:30:01 -0500

    updated project

--- client/src/Components/homepage.jsx
+++ client/src/Components/homepage.jsx
@@ -1,43 +1,12 @@
 import React from 'react'
 import '../Styles/homepage.scss'
 
+import Directory from '../Components/Directory/Directory'
+
 const Homepage = () => {
     return(
     <div className = 'homepage'>
-        <div className = 'directory-menu'>
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1 className='title'>HATS</h1>
-                    <span className = 'subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1 className='title'>Jackets</h1>
-                    <span className = 'subtitle'>SHOP NOW</span>
-                </div>
-                
-            </div>
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1 className='title'>Sneakers</h1>
-                    <span className = 'subtitle'>SHOP NOW</span>
-                </div>
-                
-            </div>
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1 className='title'>MENS</h1>
-                    <span className = 'subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className = 'menu-item'>
-                <div className = 'content'>
-                    <h1 className='title'>WOMANS</h1>
-                    <span className = 'subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+       <Directory />
     </div>
     )
 }
