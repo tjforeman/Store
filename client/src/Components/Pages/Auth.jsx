86e6a4cc90381f6e5b685d396cd30fa725321d3b
tjforeman <tylerforeman92@gmail.com>
2020-12-07 10:40:35 -0500

    ability to signup with email configured

--- client/src/Components/Pages/Auth.jsx
+++ client/src/Components/Pages/Auth.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 import  SignIn from '../Sign-in/SignIn'
+import SignUp from '../Sign-Up/SignUp'
 
 import "./Auth.scss";
 
 const Auth = () => {
-  return <div className="sign-in-and-sign-up"><SignIn /> </div>;
+  return <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  
+ </div>;
 };
 
 export default Auth;
