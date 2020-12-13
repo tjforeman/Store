a46d6ac70de029361a06b47f29fc17a5a9ff71bd
tjforeman <tylerforeman92@gmail.com>
2020-12-12 20:38:16 -0500

    formatted code

--- client/src/Components/Cart-icon/CartIcon.jsx
+++ client/src/Components/Cart-icon/CartIcon.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect'
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { toggleCartHidden } from '../../Redux/cart/cart.actions';
-import {selectCartItemsCount} from '../../Redux/cart/cart.selectors'
+import { toggleCartHidden } from "../../Redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../Redux/cart/cart.selectors";
 
-import { ReactComponent as ShoppingIcon} from '../../Assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from "../../Assets/shopping-bag.svg";
 
-import './cart-icon.scss'
+import "./cart-icon.scss";
 
-const CartIcon = ({toggleCartHidden, itemCount}) => {
-    return (
-        <div className = 'cart-icon' onClick = {toggleCartHidden}>
-            <ShoppingIcon className = 'shopping-icon' />
-    <span className = 'item-count'>{itemCount}</span>
-        </div>
-    );
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  );
 };
 
-const mapDispatchToProps = dispatch => ({
-    toggleCartHidden : () => dispatch(toggleCartHidden())
-})
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
 
-const mapStateToProps = createStructuredSelector ({
-    itemCount: selectCartItemsCount
-})
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
+});
 
-export default connect(mapStateToProps,mapDispatchToProps) (CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
