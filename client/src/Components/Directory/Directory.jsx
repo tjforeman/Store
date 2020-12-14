adfc809930861473856a48f4a61a6f97d49fc05b
tjforeman <tylerforeman92@gmail.com>
2020-12-14 13:13:33 -0500

    moved directory from local state to redux pattern

--- client/src/Components/Directory/Directory.jsx
+++ client/src/Components/Directory/Directory.jsx
@@ -1,61 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector} from 'reselect';
 
-import MenuItem from '../menu-item/menu-item'
+import { selectDirectorySections } from '../../Redux/directory/directorySelectors';
 
-import './directory.scss'
+import MenuItem from '../menu-item/menu-item';
 
-class Directory extends React.Component{
- constructor(){
- super()
- this.state = {
-    sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop'
-        }
-      ]
- }
- }   
+import './directory.scss';
 
- render(){
+const Directory = ({sections}) => { 
      return (
          <div className= 'directory-menu'>
              {
-                 this.state.sections.map(({id, ...sectionProps}) => (
+                 sections.map(({id, ...sectionProps}) => (
                      <MenuItem key={id} {...sectionProps} />
                  ))
              }
          </div>
      )
- }
 }
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
