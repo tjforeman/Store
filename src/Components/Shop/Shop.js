ed673454b75cfb5c03baa0c1df95cffbf4e11d8a
tjforeman <tylerforeman92@gmail.com>
2020-12-21 00:05:47 -0500

    wrote method to bring items from firebase

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -4,13 +4,30 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../collections-overview/CollectionsOverview";
 import Collection from "../Pages/collection/Collection";
 
-const Shop = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../Firebase/firebase.utils";
+
+class Shop extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collection");
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
+}
 
 export default Shop;
