f69a0623b6361b8e5966ac7b9b6d72a54bc03915
tjforeman <tylerforeman92@gmail.com>
2021-01-02 23:34:33 -0500

    updated spinner

--- src/Components/with-spinner/WithSpinner.jsx
+++ src/Components/with-spinner/WithSpinner.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import {SpinnerContainer, SpinnerOverlay} from './WithSpinnerStyles'
+import { SpinnerContainer, SpinnerOverlay } from "./WithSpinnerStyles";
 
-const WithSpinner = WrappedComponent =>
-{ 
-   const Spinner =  ({isLoading, ...otherProps}) => {
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer />
-        </SpinnerOverlay>
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
     ) : (
-        <WrappedComponent {... otherProps} />
-    )
-}
-    return Spinner
-}
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
+};
 
 export default WithSpinner;
